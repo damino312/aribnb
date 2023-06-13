cf213655aaff902a736b493d9f961fdda015188a
damino312 <daminionq@gmail.com>
2023-06-13 15:14:47 +0700

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,36 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#f5385d",
+  /** @type {import('tailwindcss').Config} */
+  export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#f5385d",
+        },
+        keyframes: {
+          turnAround: {
+            "100%": { transform: "rotate(360deg)" },
+          },
+          popUp: {
+            "0%": { transform: "translateY(100vh)" },
+            "100%": { transform: "translateY(0px)" },
+          },
+          fade: {
+            "0%": {
+              "@apply bg-opacity-0": {},
+            },
+            "100%": {
+              "@apply bg-opacity-50": {},
+            },
+          },
+        },
       },
     },
-  },
-  plugins: [],
-};
+    plugins: [
+      function ({ addUtilities }) {
+        addUtilities({
+          ".bg-opacity-0": { "background-color": "rgba(0,0,0,0)" },
+          ".bg-opacity-50": { "background-color": "rgba(0,0,0,0.5)" },
+        });
+      },
+    ],
+  };
