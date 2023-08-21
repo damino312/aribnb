a14c82389034931c03e0d31e07bbd11941d94c10
Popov Kirill <93064817+damino312@users.noreply.github.com>
2023-08-21 16:45:59 +0700

    Make adaptive and small changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { UserContext } from "./UserContext";
 import { PlaceContext } from "./PlaceContext";
 import axios from "axios";
@@ -9,6 +9,8 @@ export default function Header() {
   const [query, setQuery] = useState("");
   const { setPlaces } = useContext(PlaceContext);
 
+  let { pathname } = useLocation(); // To display the search bar only on IndexPage
+
   function findPlaceByName() {
     axios
       .get("/searchByPlace", { params: { title: query } })
@@ -16,18 +18,18 @@ export default function Header() {
         if (data && data.length > 0) {
           setPlaces(data);
         } else {
-          console.log("false");
+          alert("No such a place");
         }
       })
       .catch((error) => {
-        console.error("Ошибка при выполнении запроса:", error);
+        console.error("Error during executing request:", error);
       });
   }
 
   return (
     <header>
-      <div className="flex justify-between items-center ">
-        <Link to={"/"} className="flex items-center gap-1">
+      <div className="flex justify-between items-center gap-1 sm:gap-0 ">
+        <Link to={"/"} className=" items-center gap-1 flex">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -42,54 +44,44 @@ export default function Header() {
               d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
             />
           </svg>
-          <span className="font-bold text-xl">airbnb</span>
+          <span className="font-bold text-xl hidden md:inline ">airbnb</span>
         </Link>
-        <div className="flex items-center border gap-2 border-gray-300 rounded-full px-4 shadow-md shadow-gray-300">
-          <input
-            type="text"
-            value={query}
-            onChange={(ev) => setQuery(ev.target.value)}
-            className="h-7"
-            placeholder="Type the place"
-          />
-          <button
-            onClick={() => findPlaceByName(query)}
-            className="bg-primary text-white p-2 my-2 rounded-full items-center text-center"
+        {pathname.length === 1 && (
+          <div className="flex items-center border gap-2 border-gray-300 rounded-full px-3 shadow-md shadow-gray-300">
+            <input
+              type="text"
+              value={query}
+              onChange={(ev) => setQuery(ev.target.value)}
+              className="h-7"
+              placeholder="Type the place"
+            />
+            <button
+              onClick={() => findPlaceByName(query)}
+              className="bg-primary text-white p-1 my-2 rounded-full items-center text-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </button>
+          </div>
+        )}
+
+        <div className="flex items-center border gap-2 border-gray-300 rounded-full">
+          <Link
+            to={user ? "/account" : "/login"}
+            className="px-3 py-3 flex gap-2"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="flex items-center border gap-2 border-gray-300 rounded-full px-4 py-2 ">
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </button>
-          <Link to={user ? "/account" : "/login"} className="flex gap-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -102,7 +94,7 @@ export default function Header() {
                 clipRule="evenodd"
               />
             </svg>
-            {!!user && <div>{user.name}</div>}
+            {!!user && <div className="hidden sm:block">{user.name}</div>}
           </Link>
         </div>
       </div>
